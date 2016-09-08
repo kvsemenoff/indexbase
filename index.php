6a70758ac15f596abe1b415e0bd50158b834972a
Konstantin Semenov <kvsemenoff@mail.ru>
2016-09-08 19:04:59 +0400

    f

---
+++ index.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta content="telephone=no" name="format-detection"/>
+
+	<title>Главная страница</title>
+
+	<link rel="stylesheet" href="libs/bootstrap/bootstrap-grid-3.3.1.min.css" />
+	<link rel="stylesheet" href="libs/font-awesome-4.2.0/css/font-awesome.min.css" />
+	<link rel="stylesheet" href="libs/fancybox/jquery.fancybox.css" />
+	<link rel="stylesheet" href="libs/owl.carousel/assets/owl.carousel.css" />
+	<link rel="stylesheet" href="libs/wow/animate.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="shortcut icon" href="favicon.ico" />
+</head>
+<body>
+	<script src="libs/jquery/jquery-1.11.1.min.js"></script>
+	<script src="js/jquery.mousewheel.js"></script>
+	<script src="js/common.js"></script>
+	
+	<section class="header">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4 col-sm-12 col-xs-12">
+					<div class="logo-box">
+						<a href="/"><img src="img/logo.png" alt=""></a>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-12 col-xs-12">
+					<div class="header_middle">
+						<span class="tel">+7 911 745 1711</span> <br>
+						<a href="#zvonok" name="modal">Заказать обратный звонок</a>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-12 col-xs-12">
+					<div class="header-right">
+						<span>lineondesign@mail.ru</span> <br>
+						<a href="mailto:lineondesign@mail.ru">Напишите нам</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	
+
+</body>
+</html>
