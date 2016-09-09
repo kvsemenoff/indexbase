ca36e26149e8f7d2dc235e593bfd9d98d32dac81
Konstantin Semenov <kvsemenoff@mail.ru>
2016-09-09 19:01:33 +0400

    d

--- index.php
+++ index.php
@@ -272,6 +272,167 @@
 		</div>
 		
 	</div>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+
+	<div class="cases">
+		<div class="ab-wrap">
+			<h2>Кейсы CS:GO</h2>
+			<div class="case-row">
+				<div class="case-row__item">
+					<div class="img-box">
+						<img src="img/sunduk.png" alt="">
+					</div>
+					<div class="left-box">
+						<span class="bl">Bloodhound</span> <br>
+						<span class="cs">case</span>
+					</div>
+				</div>
+				<div class="case-row__item">
+					<div class="img-box">
+						<img src="img/sunduk.png" alt="">
+					</div>
+					<div class="left-box">
+						<span class="bl">Bloodhound</span> <br>
+						<span class="cs">case</span>
+					</div>
+				</div>
+				<div class="case-row__item">
+					<div class="img-box">
+						<img src="img/sunduk.png" alt="">
+					</div>
+					<div class="left-box">
+						<span class="bl">Bloodhound</span> <br>
+						<span class="cs">case</span>
+					</div>
+				</div>
+				<div class="case-row__item">
+					<div class="img-box">
+						<img src="img/sunduk.png" alt="">
+					</div>
+					<div class="left-box">
+						<span class="bl">Bloodhound</span> <br>
+						<span class="cs">case</span>
+					</div>
+				</div>
+				<div class="case-row__item">
+					<div class="img-box">
+						<img src="img/sunduk.png" alt="">
+					</div>
+					<div class="left-box">
+						<span class="bl">Bloodhound</span> <br>
+						<span class="cs">case</span>
+					</div>
+				</div>
+			</div>
+
+			<div class="case-row">
+				<div class="case-row__item">
+					<div class="img-box">
+						<img src="img/sunduk.png" alt="">
+					</div>
+					<div class="left-box">
+						<span class="bl">Bloodhound</span> <br>
+						<span class="cs">case</span>
+					</div>
+				</div>
+				<div class="case-row__item">
+					<div class="img-box">
+						<img src="img/sunduk.png" alt="">
+					</div>
+					<div class="left-box">
+						<span class="bl">Bloodhound</span> <br>
+						<span class="cs">case</span>
+					</div>
+				</div>
+				<div class="case-row__item">
+					<div class="img-box">
+						<img src="img/sunduk.png" alt="">
+					</div>
+					<div class="left-box">
+						<span class="bl">Bloodhound</span> <br>
+						<span class="cs">case</span>
+					</div>
+				</div>
+				<div class="case-row__item">
+					<div class="img-box">
+						<img src="img/sunduk.png" alt="">
+					</div>
+					<div class="left-box">
+						<span class="bl">Bloodhound</span> <br>
+						<span class="cs">case</span>
+					</div>
+				</div>
+				<div class="case-row__item">
+					<div class="img-box">
+						<img src="img/sunduk.png" alt="">
+					</div>
+					<div class="left-box">
+						<span class="bl">Bloodhound</span> <br>
+						<span class="cs">case</span>
+					</div>
+				</div>
+			</div>
+
+			<div class="case-row">
+				<div class="case-row__item">
+					<div class="img-box">
+						<img src="img/sunduk.png" alt="">
+					</div>
+					<div class="left-box">
+						<span class="bl">Bloodhound</span> <br>
+						<span class="cs">case</span>
+					</div>
+				</div>
+				<div class="case-row__item">
+					<div class="img-box">
+						<img src="img/sunduk.png" alt="">
+					</div>
+					<div class="left-box">
+						<span class="bl">Bloodhound</span> <br>
+						<span class="cs">case</span>
+					</div>
+				</div>
+				<div class="case-row__item">
+					<div class="img-box">
+						<img src="img/sunduk.png" alt="">
+					</div>
+					<div class="left-box">
+						<span class="bl">Bloodhound</span> <br>
+						<span class="cs">case</span>
+					</div>
+				</div>
+				<div class="case-row__item">
+					<div class="img-box">
+						<img src="img/sunduk.png" alt="">
+					</div>
+					<div class="left-box">
+						<span class="bl">Bloodhound</span> <br>
+						<span class="cs">case</span>
+					</div>
+				</div>
+				<div class="case-row__item">
+					<div class="img-box">
+						<img src="img/sunduk.png" alt="">
+					</div>
+					<div class="left-box">
+						<span class="bl">Bloodhound</span> <br>
+						<span class="cs">case</span>
+					</div>
+				</div>
+			</div>
+
+									
+		</div>	
+	</div>
 
 	<div class="footer-top">
 		<div class="ab-wrap">
