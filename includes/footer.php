e9bc08ace684c44f7a7f370ada2789329ed258fa
Berzeg07 <berezg86@gmail.com>
2016-09-09 18:16:53 +0300

    9.09

--- includes/footer.php
+++ includes/footer.php
@@ -1,10 +1,56 @@
 
-<div class="ab-footer">
+<!--<div class="ab-footer">
 	<div class="ab-wrap">		
 		<div class="ab-logo-footer">
-			<a href="#"><img src="img/logo-footer.png" alt=""></a><span> &copy; 2016 </span>
+			<a href="#"><img src="img/logo-footer.png" alt=""></a>
+			<span class="ab-copy-txt"> &copy; 2016 </div>
 		</div>
-
+		
+			
 
 	</div>
-</div>
+</div>-->
+
+<section class="ab-footer">
+	<div class="container">
+		<div class="row ab-classmenu">
+			<div class="col-md-2 col-sm-12 col-xs-12">
+				<div class="ab-logo-footer">
+					<a href="#"><img src="img/logo-footer.png" alt=""></a>
+				</div>
+			</div>
+
+			<div class="col-md-2 col-sm-12 col-xs-12">
+				<div class="ab-copy-txt"> &copy; 2016 </div>
+			</div>
+
+			<div class="col-md-2 col-sm-12 col-xs-12">
+				<div class="ab-social2">	
+					<a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+					<a href="#"><i class="fa fa-vk" aria-hidden="true"></i></a>	
+					<a href="#"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+				</div>
+			</div>
+
+			<div class="col-md-2 col-sm-12 col-xs-12">
+				<div class="ab-support">
+					<div class="ab-faq-footer"><a href="#">Вопросы и ответы</a></div>
+					<div class="ab-kontakt-footer"><a href="#">Контакты</a></div>
+				</div>
+			</div>
+
+			<div class="col-md-2 col-sm-12 col-xs-12">
+				<div class="ab-support">
+					<div class="ab-faq-footer"><a href="#">Пользовательское соглашение</a></div>
+					<div class="ab-kontakt-footer"><a href="#">Условия использования</a></div>
+				</div>
+			</div>
+
+			<div class="col-md-2 col-sm-12 col-xs-12">
+				<div class="ab-treug">
+					<img src="img/tr.png" alt="">
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
